ed60b6b7dbba2928cff32d6bccfb731c96219e25
SchusterPatrick <SchusterPatrick@R040_P700I3_15.paed.local>
2015-11-12 14:44:21 +0100

    Implements stuff

---
+++ src/de/gymolching/fsb/maths/Vector3.java
@@ -0,0 +1,24 @@
+package de.gymolching.fsb.maths;
+
+public class Vector3 {
+	public double x, y, z;
+
+	public Vector3(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public double getLength() {
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+	}
+	
+	public Vector3 add(Vector3 vector) {
+		return new Vector3(this.x + vector.x, this.y + vector.y, this.z + vector.z);
+	}
+
+	public void printToScreen() {
+		System.out.println("[Vector3] " + this + "\n{ " + this.x + ", "
+				+ this.y + ", " + this.z + " }");
+	}
+}
